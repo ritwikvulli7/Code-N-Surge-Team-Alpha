aad32192731f8e531c47da1a0ed10cd8121ae160
ritwikvulli7 <ritwikvulli7@gmail.com>
2024-09-24 11:35:27 +0530

    ganesh

--- app.js
+++ app.js
@@ -4,17 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/news', async (req, res) => {
   try {
     const apiKey = '9988ad3ffb2f4c9693a6abba980b09a2'; 
-    const category = req.query.category || ''; // Get the category from the query parameter
+    const category = req.query.category || '';
     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`;
     
     const response = await axios.get(apiUrl);
-    res.json(response.data); // Send the fetched news data to the frontend
+    res.json(response.data); 
   } catch (error) {
     res.status(500).send('Error fetching news');
   }
