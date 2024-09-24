5cdfec692524d477c9a17df0f8420e0bb66ed8a1
ritwikvulli7 <ritwikvulli7@gmail.com>
2024-09-24 10:43:14 +0530

    ravikanth

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const PORT = 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Endpoint to get news data from NewsAPI
 app.get('/news', async (req, res) => {
   try {
-    const apiKey = '9988ad3ffb2f4c9693a6abba980b09a2'; // Replace with your NewsAPI key
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+    const apiKey = '9988ad3ffb2f4c9693a6abba980b09a2'; 
+    const category = req.query.category || ''; // Get the category from the query parameter
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`;
+    
     const response = await axios.get(apiUrl);
     res.json(response.data); // Send the fetched news data to the frontend
   } catch (error) {
@@ -19,12 +20,11 @@ app.get('/news', async (req, res) => {
   }
 });
 
-// Fallback: Catch-all route to serve index.html for all routes
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
